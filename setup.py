f8881b0ae1d0c2159a798455a56c7391d6ac8d54
Matthew Peters <matthewp@allenai.org>
2019-01-16 13:12:40 -0800

    Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(
+    name             = 'bert',
+    version          = '0',
+    packages         = ['bert'],
+    package_dir      = {'bert':'.'},
+)
+
+
